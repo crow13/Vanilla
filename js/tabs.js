8188ee33239be2d2f9f014f32de47656c8b5387e
Joseph Whitaker <joseph.carl.whitaker@gmail.com>
2012-09-10 13:33:31 -0400

    styled tabs.

--- js/tabs.js
+++ js/tabs.js
@@ -7,9 +7,14 @@ toggleAccordion = function(e){
 	var targetItemId = e.currentTarget.id.replace("header", "content");
 	var targetItemContent = document.getElementById(targetItemId);
 	for(var i = 0; i<contentBlocks.length; i++){
+        removeClass(headerBlocks[i],'greyBkgrd');
+        addClass(headerBlocks[i],'whtBkgrd');
 		removeClass(contentBlocks[i], 'show');
 		addClass(contentBlocks[i],'hide');
 	}
+
+    removeClass(e.currentTarget, 'whtBkgrd');
+    addClass(e.currentTarget, 'greyBkgrd');
 	removeClass(targetItemContent, 'hide');
 	addClass(targetItemContent,'show');
 }
